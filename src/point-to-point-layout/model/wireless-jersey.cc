38a29f4e6254f8ea2e9e2576e8570dc793010245
Sourabh Jain <sourabhjain560@outlook.com>
2017-04-12 22:00:47 +0530

    Added Topology Helper for Jersey

---
+++ src/point-to-point-layout/model/wireless-jersey.cc
@@ -0,0 +1,210 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Sourabh Jain <jainsourabh679@gmail.com>
+ *         Vivek Jain	<jain.vivek.anand@gmail.com>
+ */
+
+
+#include <cmath>
+#include <iostream>
+#include <sstream>
+
+// ns3 includes
+#include "ns3/log.h"
+
+#include "ns3/constant-position-mobility-model.h"
+
+#include "ns3/node-list.h"
+#include "ns3/point-to-point-net-device.h"
+#include "ns3/vector.h"
+#include "ns3/ipv6-address-generator.h"
+
+#include "ns3/mobility-module.h"
+#include "ns3/config-store-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/wireless-jersey.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("WirelessJerseyHelper");
+
+WirelessJerseyHelper::WirelessJerseyHelper (uint32_t nLeftLeaf,
+                                            uint32_t nRightLeaf,
+                                            PointToPointHelper leftHelper,
+                                            PointToPointHelper rightHelper,
+					    PointToPointHelper bottleneckHelper,
+                                            std::string mobilityModel)
+{
+  
+  m_centralNodes.Create(3);
+  NodeContainer nc1;
+  nc1.Add (m_centralNodes.Get (0));
+  nc1.Add (m_centralNodes.Get (1));
+  
+  NodeContainer nc2;
+  nc2.Add (m_centralNodes.Get (1));
+  nc2.Add (m_centralNodes.Get (2));
+  
+  // Create the leaf nodes
+  m_leftLeaf.Create (nLeftLeaf);
+  
+  // Create the right nodes
+  m_rightLeaf.Create (nRightLeaf);
+
+  // Add the link connecting routers
+  leftToMiddleDevice = bottleneckHelper.Install (nc1);
+  
+  YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
+  YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
+  phy.SetChannel (channel.Create ());
+
+  WifiHelper wifi;
+  wifi.SetStandard (WIFI_PHY_STANDARD_80211b);
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+                                "DataMode", StringValue ("DsssRate2Mbps"),
+                                "ControlMode", StringValue ("DsssRate1Mbps"));
+
+  WifiMacHelper mac;
+  mac.SetType ("ns3::AdhocWifiMac");
+  
+  MobilityHelper mobility;
+  mobility.SetMobilityModel (mobilityModel);
+  mobility.Install (nc2);
+
+  NetDeviceContainer devices;
+  devices = wifi.Install (phy, mac, nc2);
+  
+  middleToRightDevice.Add (devices);
+
+
+  
+  // Add the left side links
+  for(uint32_t i = 0; i < nLeftLeaf;  ++i)
+  {
+        NetDeviceContainer c = leftHelper.Install (m_centralNodes.Get (0),
+                                          m_leftLeaf.Get (i));
+        m_leftRouterDevices.Add (c.Get (0));
+        m_leftLeafDevices.Add (c.Get (1));
+  }
+  
+  
+  // Add the right side links
+  for (uint32_t i = 0; i < nRightLeaf; ++i)
+    {
+      NetDeviceContainer c = rightHelper.Install (m_centralNodes.Get (2),
+                                                  m_rightLeaf.Get (i));
+      m_rightRouterDevices.Add (c.Get (0));
+      m_rightLeafDevices.Add (c.Get (1));
+    }
+}
+
+WirelessJerseyHelper::~WirelessJerseyHelper ()
+{
+
+}
+
+Ptr<Node> WirelessJerseyHelper::GetLeft () const
+{ // Get the left side bottleneck router
+  return m_centralNodes.Get (0);
+}
+
+Ptr<Node> WirelessJerseyHelper::GetLeft (uint32_t i) const
+{ // Get the i'th left side leaf
+  return m_leftLeaf.Get (i);
+}
+
+Ptr<Node> WirelessJerseyHelper::GetRight () const
+{ // Get the right side bottleneck router
+  return m_centralNodes.Get (2);
+}
+
+Ptr<Node> WirelessJerseyHelper::GetRight (uint32_t i) const
+{ // Get the i'th right side leaf
+  return m_rightLeaf.Get (i);
+}
+
+Ptr<Node> WirelessJerseyHelper::GetMiddle () const
+{ 
+  return m_centralNodes.Get (1);
+}
+
+Ipv4Address WirelessJerseyHelper::GetLeftIpv4Address (uint32_t i) const
+{
+  return m_leftLeafInterfaces.GetAddress (i);
+}
+
+Ipv4Address WirelessJerseyHelper::GetRightIpv4Address (uint32_t i) const
+{
+  return m_rightLeafInterfaces.GetAddress (i);
+}
+
+uint32_t  WirelessJerseyHelper::LeftCount () const
+{ // Number of left side nodes
+  return m_leftLeaf.GetN ();
+}
+
+uint32_t  WirelessJerseyHelper::RightCount () const
+{ // Number of right side nodes
+  return m_rightLeaf.GetN ();
+}
+
+void WirelessJerseyHelper::InstallStack (InternetStackHelper stack)
+{
+  stack.Install (m_centralNodes);
+  stack.Install (m_leftLeaf);
+  stack.Install (m_rightLeaf);
+  
+
+}
+
+void WirelessJerseyHelper::AssignIpv4Addresses (Ipv4AddressHelper leftIp,
+                                                Ipv4AddressHelper rightIp,
+                                                Ipv4AddressHelper leftToMidddleIp,
+                                                Ipv4AddressHelper middleToRightIp)
+{
+
+  // Assign to left side 
+  NetDeviceContainer ndc1;
+  ndc1.Add (m_leftLeafDevices);
+  ndc1.Add(m_leftRouterDevices);
+  Ipv4InterfaceContainer ifc1 = leftIp.Assign (ndc1);
+  
+   for (uint32_t i = 0; i < LeftCount (); ++i)
+   {
+      m_leftLeafInterfaces.Add (ifc1.Get (i));
+    }
+  m_leftRouterInterfaces.Add (ifc1.Get (LeftCount()));
+  leftIp.NewNetwork ();
+  
+  
+  // Assign to right side 
+  NetDeviceContainer ndc2;
+  ndc2.Add (m_rightLeafDevices);
+  ndc2.Add(m_rightRouterDevices);
+  Ipv4InterfaceContainer ifc2 = rightIp.Assign (ndc2);
+  
+   for (uint32_t i = 0; i < RightCount (); ++i)
+   {
+      m_rightLeafInterfaces.Add (ifc2.Get (i));
+    }
+  m_rightRouterInterfaces.Add (ifc2.Get (LeftCount()));
+  leftIp.NewNetwork ();
+  
+  m_middleRouterToLeftInterfaces = leftToMidddleIp.Assign (leftToMiddleDevice);
+  m_middleRouterToRightInterfaces = middleToRightIp.Assign (middleToRightDevice);
+}
+
+} // namespace ns3
---
+++ src/point-to-point-layout/model/wireless-jersey.h
@@ -0,0 +1,159 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Sourabh Jain <jainsourabh679@gmail.com>
+ *         Vivek Jain	<jain.vivek.anand@gmail.com>
+ */
+
+
+
+#include <string>
+
+#include "point-to-point-helper.h"
+#include "ipv4-address-helper.h"
+#include "ipv6-address-helper.h"
+#include "internet-stack-helper.h"
+#include "ipv4-interface-container.h"
+#include "ipv6-interface-container.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/athstats-helper.h"
+#include "ns3/config-store-module.h"
+
+namespace ns3 {
+
+class WirelessJerseyHelper
+{
+public:
+
+  WirelessJerseyHelper (uint32_t nLeftLeaf,
+                        uint32_t nRightLeaf,
+                        PointToPointHelper rightHelpe,
+                        PointToPointHelper leftHelper,    
+                        PointToPointHelper centrallinkHelper,
+			std::string mobilityModel);
+
+  ~WirelessJerseyHelper ();
+
+public:
+  /**
+   * \returns pointer to the node of the left side bottleneck
+   *          router
+   */
+  Ptr<Node> GetLeft () const;
+
+  /**
+   * \returns pointer to the i'th left side leaf node
+   * \param i node number
+   */
+  Ptr<Node> GetLeft (uint32_t i) const;
+
+  /**
+   * \returns pointer to the node of the right side bottleneck
+   *          router
+   */
+  Ptr<Node> GetRight () const;
+
+  /**
+   * \returns pointer to the i'th right side leaf node
+   * \param i node number
+   */
+  Ptr<Node> GetRight (uint32_t i) const;
+
+  /**
+   * \returns pointer to the middle router
+   */
+  Ptr<Node> GetMiddle () const;
+
+  /**
+   * \returns an Ipv4Address of the i'th left leaf
+   * \param i node number
+   */
+  Ipv4Address GetLeftIpv4Address (uint32_t i ) const; // Get left leaf address
+
+  /**
+   * \returns an Ipv4Address of the i'th right leaf
+   * \param i node number
+   */
+  Ipv4Address GetRightIpv4Address (uint32_t i) const;
+
+  /**
+   * \returns total number of left side leaf nodes
+   */
+  uint32_t  LeftCount () const;
+
+  /**
+   * \returns total number of right side leaf nodes
+   */
+  uint32_t  RightCount () const;
+
+  /**
+   * \param stack an InternetStackHelper which is used to install 
+   *              on every node in the wireless-jersey
+   */
+  void      InstallStack (InternetStackHelper stack);
+
+  /**
+   * \param leftIp Ipv4AddressHelper to assign Ipv4 addresses to the
+   *               interfaces on the left side of the wireless-jersey
+   *
+   * \param rightIp Ipv4AddressHelper to assign Ipv4 addresses to the
+   *                interfaces on the right side of the wireless-jersey
+   *
+   * \param leftToMiddleRotuerIP Ipv4AddressHelper to assign Ipv4 addresses to the 
+   *                 interfaces between left to middle router
+   *
+   * \param middleToRightIp Ipv4AddressHelper to assign Ipv4 addresses to the 
+   *                 interfaces between middle router to right
+   */
+void AssignIpv4Addresses (Ipv4AddressHelper leftIp,
+                          Ipv4AddressHelper rightIp,
+                          Ipv4AddressHelper leftToMidddleIp,
+                          Ipv4AddressHelper middleToRightIp);
+
+
+
+private:
+  NodeContainer          m_leftLeaf;                           //!< Left Leaf nodes
+  NetDeviceContainer     m_leftLeafDevices;                    //!< Left Leaf NetDevices
+
+  NodeContainer          m_rightLeaf;                          //!< Right Leaf nodes
+  NetDeviceContainer     m_rightLeafDevices;                   //!< Right Leaf NetDevices
+  
+  NodeContainer          m_centralNodes;        
+
+  NetDeviceContainer     m_leftRouterDevices;                 //!< Routers NetDevices
+  NetDeviceContainer     m_rightRouterDevices;                //!< Right router NetDevices
+
+  Ipv4InterfaceContainer m_leftLeafInterfaces;                //!< Left Leaf interfaces (IPv4)
+  Ipv4InterfaceContainer m_leftRouterInterfaces;              //!< Left router interfaces (IPv4)
+  Ipv4InterfaceContainer m_rightLeafInterfaces;               //!< Right Leaf interfaces (IPv4)
+  Ipv4InterfaceContainer m_rightRouterInterfaces;             //!< Right router interfaces (IPv4)
+  Ipv4InterfaceContainer m_middleRouterToLeftInterfaces;      //!< Router interfaces (IPv4)
+  Ipv4InterfaceContainer m_middleRouterToRightInterfaces;
+
+  WifiHelper wifi;
+  MobilityHelper mobility;
+  WifiMacHelper wifiMac;
+  YansWifiPhyHelper wifiPhy;
+  YansWifiChannelHelper wifiChannel;
+  NetDeviceContainer leftToMiddleDevice;
+  NetDeviceContainer middleToRightDevice;
+};
+
+} // namespace ns3
